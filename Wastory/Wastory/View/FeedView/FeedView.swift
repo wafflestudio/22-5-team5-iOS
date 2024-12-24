dcc3e56a7c8ce81e4f8a6ce808d3529dadeabd75
JoongWon SHIN <joongwon0204@gmail.com>
2024-12-25 01:01:39 +0900

    Feat: Create FeedView.swift

---
+++ Wastory/Wastory/View/FeedView/FeedView.swift
@@ -0,0 +1,19 @@
+//
+//  FeedView.swift
+//  Wastory
+//
+//  Created by 중워니 on 12/25/24.
+//
+
+import SwiftUI
+
+//"피드" View에 구독 중인 블로그의 최신 글을 표시
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
