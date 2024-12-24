9ac6493206e5c158623db07647f99467bf2eef10
JoongWon SHIN <joongwon0204@gmail.com>
2024-12-24 14:24:29 +0900

    init

---
+++ Wastory/Wastory/WastoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WastoryApp.swift
+//  Wastory
+//
+//  Created by 중워니 on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WastoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
