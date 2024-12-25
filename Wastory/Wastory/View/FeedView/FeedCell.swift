732668e7b63dd6112da7d8f2fe24231cfd128e3f
JoongWon SHIN <joongwon0204@gmail.com>
2024-12-25 12:49:36 +0900

    Feat: Add FeedCell & Add Post Texts

---
+++ Wastory/Wastory/View/FeedView/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  Wastory
+//
+//  Created by 중워니 on 12/25/24.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}
