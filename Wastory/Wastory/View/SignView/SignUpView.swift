1b483818003e053d8fc2285cee7d8a0487b1786f
muzigae <mujigae1020@gmail.com>
2024-12-27 04:15:48 +0900

    Feat: Create view & viewModel for sign up

---
+++ Wastory/Wastory/View/SignView/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Wastory
+//
+//  Created by mujigae on 12/27/24.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    @State private var viewModel = SignUpViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("회원가입")
+        }
+    }
+}
