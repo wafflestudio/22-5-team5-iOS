dff7dd544742bbe41ff73381c4834866909fce6f
muzigae <mujigae1020@gmail.com>
2024-12-27 03:49:09 +0900

    Feat: Modify initial execution structure

--- Wastory/Wastory/WastoryApp.swift
+++ Wastory/Wastory/WastoryApp.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 @main
 struct WastoryApp: App {
+    @AppStorage("userID") private var userID: String = ""
+    @AppStorage("userPW") private var userPW: String = ""
+    @State private var isLoading: Bool = true
+    
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            if isLoading {
+                LoadingView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            self.isLoading = false
+                        }
+                    }
+            }
+            else {
+                if userID.isEmpty {
+                    SignInView()
+                }
+                else {
+                    // 현재는 ID, PW가 있을 경우 자동 로그인이지만 나중에는 PW를 DB에 있는 PW와 확인하는 절차가 필요
+                    MainTabView()
+                }
+            }
         }
     }
 }
