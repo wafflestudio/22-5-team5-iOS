58c17dbf3b896f71b4d969f07e0dbb6f18011254
JoongWon SHIN <joongwon0204@gmail.com>
2024-12-30 00:00:10 +0900

    Refactor: Modify Navbar title & button

--- Wastory/Wastory/View/FeedView/FeedView.swift
+++ Wastory/Wastory/View/FeedView/FeedView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 //"피드" View에 구독 중인 블로그의 최신 글을 표시
 struct FeedView: View {
+    @State var viewModel = FeedViewModel()
     
     @State var subscribingCount: Int = 0 // 구독중 count
     @State var subscriberCount:  Int = 0 // 구독자 count
@@ -20,9 +21,22 @@ struct FeedView: View {
     var body: some View {
         VStack(spacing: 0) {
             ScrollView {
+                GeometryReader { geometry in
+                    Color.clear
+                        .onChange(of: geometry.frame(in: .global).minY) { newValue, oldValue in
+                            // 스크롤 오프셋이 네비게이션 바 아래로 들어가면 텍스트 숨김
+                            viewModel.changeIsNavTitleHidden(by: newValue)
+                        }
+                }
+                .frame(height: 0)
                 
-                // MARK: subCountTexts
+                // MARK: 화면 상단 구성 요소
                 HStack {
+                    //Navbar title
+                    Text("피드")
+                        .font(.system(size: 34, weight: .bold))
+                        .padding(.leading)
+                    
                     Spacer()
                     
                     // 구독중
@@ -50,9 +64,7 @@ struct FeedView: View {
                             .foregroundStyle(Color.black)
                     }
                 }
-                .offset(y: -50) // NavBar 위로 올라가도록 offset 조정
                 .padding(.trailing, 22)
-                .padding(.bottom, -50)
                 
                 //MARK: PostList
                 LazyVStack(spacing: 0) {
@@ -66,10 +78,19 @@ struct FeedView: View {
         }
         // MARK: NavBar
         // TODO: rightTabButton - 검색버튼과 본인계정버튼은 4개의 TabView에 공통 적용이므로 추후 제작
-        .navigationTitle(Text("피드"))
-        .navigationBarTitleDisplayMode(.large)
-//        .toolbarBackground(Color.blue, for: .navigationBar)
-//        .toolbarBackground(.visible, for: .navigationBar)       // NavBar size 테스트 용도
+        .navigationTitle(Text(viewModel.getIsNavTitleHidden() ? "피드" : ""))
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbarBackgroundVisibility(.automatic, for: .navigationBar)
+        .toolbarBackground(Color.white, for: .navigationBar)
+        .toolbar{
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button{
+                    
+                } label: {
+                    Text(Image(systemName: "magnifyingglass"))
+                }
+            } // navbar 사이즈 설정을 위한 임의 버튼입니다.
+        }
                 
         
     }
--- Wastory/Wastory/ViewModel/FeedViewModel/FeedViewModel.swift
+++ Wastory/Wastory/ViewModel/FeedViewModel/FeedViewModel.swift
@@ -10,4 +10,23 @@
 import SwiftUI
 import Observation
 
-@Observable final class SignInViewModel {
+@Observable final class FeedViewModel {
+    private var isNavTitleHidden: Bool = false
+    
+    func changeIsNavTitleHidden(by newValue: CGFloat) {
+        if newValue <= 60 {
+            if (!isNavTitleHidden) {
+                isNavTitleHidden = true
+            }
+        } else {
+            if (isNavTitleHidden) {
+                isNavTitleHidden = false
+            }
+        }
+    }
+    
+    func getIsNavTitleHidden() -> Bool {
+        isNavTitleHidden
+    }
+    
+}
