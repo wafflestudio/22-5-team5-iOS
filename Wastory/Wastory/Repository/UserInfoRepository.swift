6011cb8fae2880c875277d177e26e96d0a89d124
muzigae <mujigae1020@gmail.com>
2024-12-30 00:08:26 +0900

    Feat: Separate user information setting functions

--- Wastory/Wastory/Repository/UserInfoRepository.swift
+++ Wastory/Wastory/Repository/UserInfoRepository.swift
@@ -15,9 +15,16 @@ final class UserInfoRepository {
     private var userPW = ""     // 비밀번호
     private var username = ""   // 닉네임 (블로그 주소 대체)
     
-    func setUserInfo(userID: String, userPW: String) {  // 회원가입 시 이용
+    func setUserID(userID: String) {        // 회원가입 시 이용
         self.userID = userID
+    }
+    
+    func setUserPW(userPW: String) {        // 회원가입 시 이용
         self.userPW = userPW
+    }
+    
+    func setUsername(username: String) {    // 회원가입 시 이용
+        self.username = username
     }
     
     func loadUserInfo(userID: String, userPW: String) { // 로그인 시 이용
