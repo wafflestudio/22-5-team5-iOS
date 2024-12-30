fe9a8dd400207aba46cfe508354e3e7417170e21
muzigae <mujigae1020@gmail.com>
2024-12-30 13:08:11 +0900

    Feat: Add login failure variable

--- Wastory/Wastory/ViewModel/SignViewModel/SignInViewModel.swift
+++ Wastory/Wastory/ViewModel/SignViewModel/SignInViewModel.swift
@@ -13,10 +13,13 @@ import Observation
     @AppStorage("userPW") @ObservationIgnored private var userPW: String = ""
     @AppStorage("loginAutoSave") @ObservationIgnored private var loginInfoSave: Bool = false
     
+    private var userInfoRepository = UserInfoRepository.shared
+    
     var id = ""
     var password = ""
     private var isLoginInfoSave = false
     private var isPasswordInvisible: Bool = true
+    private var loginFailed = false
     
     init() {
         id = userID
@@ -24,6 +27,8 @@ import Observation
     }
     
     func login() {
+        // DB와 통신하여 로그인 성공 여부를 판단하는 기능 필요
+        
         loginInfoSave = isLoginInfoSave
         if loginInfoSave {
             userID = id
@@ -33,6 +38,15 @@ import Observation
             userID = ""
             userPW = ""
         }
+        userInfoRepository.loadUserInfo(userID: userID, userPW: userPW)
+    }
+    
+    func isLoginFailed() -> Bool {
+        return loginFailed
+    }
+    
+    func toggleLoginFailed() {  // 로그인 실패 문구를 확인하기 위한 임시 함수 (추후 삭제 예정)
+        loginFailed.toggle()
     }
     
     func toggleAutoSave() {
