95866979b3505cb305179d29f19397aa898ac5d3
muzigae <mujigae1020@gmail.com>
2024-12-31 09:17:26 +0900

    Feat: Create sign up step2 view & view model - email view

---
+++ Wastory/Wastory/View/SignView/SignUpStep2EmailView.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpStep2EmailView.swift
+//  Wastory
+//
+//  Created by mujigae on 12/30/24.
+//
+
+import SwiftUI
+
+struct SignUpStep2EmailView: View {
+    @State private var viewModel = SignUpStep2ViewModel()
+    
+    var body: some View {
+        Text("이메일 인증 뷰")
+    }
+}
