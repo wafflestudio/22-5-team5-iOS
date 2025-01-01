59c7e88f5024d0d5f33231a2becf5ca5a3085b43
muzigae <mujigae1020@gmail.com>
2025-01-02 06:07:03 +0900

    Feat: Add sign up step3 view & view model - password view

---
+++ Wastory/Wastory/View/SignView/SignUpStep3PasswordView.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpStep3PasswordView.swift
+//  Wastory
+//
+//  Created by mujigae on 1/2/25.
+//
+
+import SwiftUI
+
+struct SignUpStep3PasswordView: View {
+    @State private var viewModel = SignUpStep3ViewModel()
+    
+    var body: some View {
+        Text("비밀번호 설정 뷰")
+    }
+}
