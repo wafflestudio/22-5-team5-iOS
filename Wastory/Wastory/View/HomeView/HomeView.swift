b15203dfe61256c76a1051b4f88885925b7f7b02
JoongWon SHIN <joongwon0204@gmail.com>
2025-01-05 14:18:59 +0900

    Feat: Add TodaysWastoryCell

--- Wastory/Wastory/View/HomeView/HomeView.swift
+++ Wastory/Wastory/View/HomeView/HomeView.swift
@@ -9,17 +9,33 @@ import SwiftUI
 
 struct HomeView: View {
     
-    var todaysWastoryItems = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+    var todaysWastoryItems = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
     
     
     var body: some View {
-        VStack(spacing: 0) {
-            // MARK: 오늘의 티스토리
-            TabView {
-                ForEach(todaysWastoryItems, id: \.self) { item in
-                    TodaysWastoryCell()
+        ScrollView {
+            VStack(spacing: 0) {
+                // MARK: 오늘의 티스토리
+                TabView {
+                    ForEach(todaysWastoryItems, id: \.self) { item in
+                        TodaysWastoryCell()
+                    }
                 }
-            }
-        }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                .frame(height: 300)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(.horizontal, 20)
+                
+                
+                
+            } //VStack
+        } //ScrollView
+        
     }
+    
+}
+
+
+#Preview {
+    HomeView()
 }
--- Wastory/Wastory/View/HomeView/TodaysWastoryCell.swift
+++ Wastory/Wastory/View/HomeView/TodaysWastoryCell.swift
@@ -9,12 +9,90 @@ import SwiftUI
 
 struct TodaysWastoryCell: View {
     var body: some View {
-        VStack(spacing: 0) {
+        ZStack {
+            //Background Image
+            GeometryReader { geometry in
+                Image(systemName: "questionmark.text.page.fill")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geometry.size.width ,height: geometry.size.height)
+                    .clipped()
+                    .foregroundStyle(Color.unreadNotification)
+            }
             
+            //Background Dimming
+            Color.sheetOuterBackgroundColor
+            
+            
+            VStack(spacing: 0) {
+                HStack {
+                    Spacer()
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.todaysWastoryTextOutterBoxColor, lineWidth: 1.5)
+                            .frame(width: 90, height: 26)
+                        
+                        Text("오늘의 티스토리")
+                            .font(.system(size: 11, weight: .light))
+                            .foregroundStyle(Color.todaysWastoryTextColor)
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 23)
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    HStack(spacing: 0) {
+                        Image(systemName: "quote.opening")
+                            .font(.system(size: 26, weight: .semibold))
+                            .foregroundStyle(Color.todaysWastoryTextColor)
+                        Spacer()
+                    }
+                    .padding(.bottom, 5)
+                    
+                    Text("제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목제목")
+                        .font(.system(size: 23, weight: .semibold))
+                        .foregroundStyle(Color.todaysWastoryTextColor)
+                        .lineLimit(3)
+                        .padding(.bottom, 15)
+                    
+                    HStack {
+                        Button(action: {
+                            
+                        }) {
+                            ZStack {
+                                Image(systemName: "questionmark.text.page.fill")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .clipShape(Circle())
+                                
+                                Circle()
+                                    .stroke(Color.todaysWastoryTextColor, lineWidth: 1.7)
+                            }
+                            .frame(width: 23, height: 23)
+                            
+                            Text("블로그이름")
+                                .font(.system(size: 14, weight: .semibold))
+                                .foregroundStyle(Color.todaysWastoryTextColor)
+                        }
+                        Spacer()
+                    }
+                    .padding(.bottom, 22)
+                }
+                .padding(.horizontal, 25)
+            }
         }
+        .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
 #Preview {
     TodaysWastoryCell()
+}
+
+extension Color {
+    static let todaysWastoryTextOutterBoxColor = Color.white.opacity(0.5)
+    static let todaysWastoryTextColor = Color.white
 }
