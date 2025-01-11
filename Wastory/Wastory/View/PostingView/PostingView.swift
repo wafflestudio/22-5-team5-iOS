a8599b57999adb49f95e8fd0513195fb6b4b06a1
muzigae <mujigae1020@gmail.com>
2025-01-11 13:48:37 +0900

    Feat: Implement multi-line text editor

--- Wastory/Wastory/View/PostingView/PostingView.swift
+++ Wastory/Wastory/View/PostingView/PostingView.swift
@@ -8,29 +8,41 @@
 import SwiftUI
 
 struct PostingView: View {
-    
     @Bindable var mainTabViewModel: MainTabViewModel
-    @State var viewModel = PostingViewModel()
+    @State private var viewModel = PostingViewModel()
+    @State private var richText = NSAttributedString()
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 0) {
+            VStack {
                 // MARK: Title TextField
                 TextField("제목", text: $viewModel.title)
                     .font(.system(size: 26, weight: .regular))
                     .foregroundStyle(Color.primaryLabelColor)
+                    .padding(.top, 20)
+                    .padding(.horizontal, 20)
                 
                 Spacer()
                     .frame(height: 10)
                 
                 // MARK: Contents TextField
-                TextField("내용을 입력해주세요", text: $viewModel.content)
-                    .font(.system(size: 14, weight: .regular))
-                    .foregroundStyle(Color.primaryLabelColor)
-                    
+                ZStack(alignment: .topLeading) {
+                    if richText.string.isEmpty {
+                        HStack {
+                            Text("내용을 입력해주세요.")
+                                .font(.system(size: 15, weight: .regular))
+                                .foregroundStyle(Color.promptLabelColor)
+                                .padding(.horizontal, 20)
+                                .padding(.top, 12)
+                            Spacer()
+                        }
+                    }
+                    RichTextEditor(text: $richText)
+                        .frame(height: 300)
+                        .padding(.horizontal, 15)
+                }
+                Spacer()
             }
-            .padding(.horizontal, 20)
-            .padding(.vertical, 20)
         }
         .toolbarBackgroundVisibility(.visible)
         .toolbarBackground(Color.white)
--- Wastory/Wastory/View/PostingView/RichTextEditor.swift
+++ Wastory/Wastory/View/PostingView/RichTextEditor.swift
@@ -9,13 +9,43 @@ import SwiftUI
 import UIKit
 
 struct RichTextEditor: UIViewRepresentable {
+    @Binding var text: NSAttributedString
     
     func updateUIView(_ uiView: UITextView, context: Context) {
     }
     
-    @State var text: NSAttributedString
-    
     func makeUIView(context: Context) -> UITextView {
-        return UITextView()
+        let textView = UITextView()
+        textView.backgroundColor = .clear
+        
+        let initialFont = UIFont.systemFont(ofSize: 15)
+        let initialParagraphStyle = NSMutableParagraphStyle()
+        initialParagraphStyle.lineSpacing = 8
+        let initialAttributes = [
+            NSAttributedString.Key.font: initialFont,
+            NSAttributedString.Key.paragraphStyle: initialParagraphStyle
+        ]
+        textView.typingAttributes = initialAttributes
+        
+        textView.isEditable = true
+        textView.isScrollEnabled = true
+        textView.delegate = context.coordinator
+        return textView
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UITextViewDelegate {
+        var parent: RichTextEditor
+        
+        init(_ parent: RichTextEditor) {
+            self.parent = parent
+        }
+        
+        func textViewDidChange(_ textView: UITextView) {
+            parent.text = textView.attributedText
+        }
     }
 }
