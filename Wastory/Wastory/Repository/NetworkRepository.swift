a62f089403ebc1a254e429aaf00f757175951537
JoongWon SHIN <joongwon0204@gmail.com>
2025-01-21 15:29:08 +0900

    Feat: API 수정사항 반영

--- Wastory/Wastory/Repository/NetworkRepository.swift
+++ Wastory/Wastory/Repository/NetworkRepository.swift
@@ -5,144 +5,6 @@
 //  Created by mujigae on 1/7/25.
 //
 
-//import Foundation
-//import Alamofire
-//
-//final class NetworkRepository {
-//    static let shared = NetworkRepository()    // 싱글톤 인스턴스
-//    
-//    // MARK: User
-//    func postSignUp(userID: String, userPW: String) async throws {
-//        let requestBody = [
-//            "email": userID,
-//            "password": userPW
-//        ]
-//        var urlRequest = try URLRequest(
-//            url: NetworkRouter.postSignUp.url,
-//            method: NetworkRouter.postSignUp.method,
-//            headers: NetworkRouter.postSignUp.headers
-//        )
-//        urlRequest.httpBody = try JSONEncoder().encode(requestBody)
-//        
-//        _ = try await AF.request(urlRequest)
-//            .validate()
-//            .serializingString().value
-//    }
-//    
-//    func postSignIn(userID: String, userPW: String) async throws -> TokenDto {
-//        let requestBody = [
-//            "email": userID,
-//            "password": userPW
-//        ]
-//        var urlRequest = try URLRequest(
-//            url: NetworkRouter.postSignIn.url,
-//            method: NetworkRouter.postSignIn.method,
-//            headers: NetworkRouter.postSignIn.headers
-//        )
-//        urlRequest.httpBody = try JSONEncoder().encode(requestBody)
-//        
-//        let response = try await AF.request(urlRequest)
-//            .validate()
-//            .serializingDecodable(TokenDto.self).value
-//        
-//        return response
-//    }
-//    
-//    // MARK: Blog
-//    func postBlog(addressName: String) async throws {
-//        let requestBody = [
-//            "address_name": addressName
-//        ]
-//        var urlRequest = try URLRequest(
-//            url: NetworkRouter.postBlog.url,
-//            method: NetworkRouter.postBlog.method,
-//            headers: NetworkRouter.postBlog.headers
-//        )
-//        urlRequest.httpBody = try JSONEncoder().encode(requestBody)
-//        
-//        _ = try await AF.request(
-//            urlRequest,
-//            interceptor: NetworkInterceptor()
-//        ).validate().serializingDecodable(Blog.self).value
-//    }
-//    
-//    func getMyBlog() async throws -> Blog {
-//        let urlRequest = try URLRequest(
-//            url: NetworkRouter.getMyBlog.url,
-//            method: NetworkRouter.getMyBlog.method,
-//            headers: NetworkRouter.getMyBlog.headers
-//        )
-//        
-//        let response = try await AF.request(
-//            urlRequest,
-//            interceptor: NetworkInterceptor()
-//        ).validate().serializingDecodable(Blog.self).value
-//        
-//        return response
-//    }
-//    
-//    func getBlog(blogAddress: String) async throws -> Blog {
-//        let urlRequest = try URLRequest(
-//            url: NetworkRouter.getBlog(blogAddress: blogAddress).url,
-//            method: NetworkRouter.getBlog(blogAddress: blogAddress).method,
-//            headers: NetworkRouter.getBlog(blogAddress: blogAddress).headers
-//        )
-//        
-//        let response = try await AF.request(
-//            urlRequest,
-//            interceptor: NetworkInterceptor()
-//        ).validate().serializingDecodable(Blog.self).value
-//        
-//        return response
-//    }
-//    
-//    // MARK: Article
-//    func postArticle(title: String, content: String, description: String, categoryID: Int) async throws {
-//        let requestBody = [
-//            "title": title,
-//            "content": content,
-//            "description": description,
-//            "category_id": "\(categoryID)"
-//        ]
-//        var urlRequest = try URLRequest(
-//            url: NetworkRouter.postArticle.url,
-//            method: NetworkRouter.postArticle.method,
-//            headers: NetworkRouter.postArticle.headers
-//        )
-//        urlRequest.httpBody = try JSONEncoder().encode(requestBody)
-//        
-//        _ = try await AF.request(
-//            urlRequest,
-//            interceptor: NetworkInterceptor()
-//        ).validate().serializingDecodable(Post.self).value
-//    }
-//    
-//    func getArticlesInBlog(blogID: Int, page: Int) async throws -> [Post] {
-//        
-//        var urlRequest = try URLRequest(
-//            url: NetworkRouter.getArticlesInBlog(blogID: blogID).url,
-//            method: NetworkRouter.getArticlesInBlog(blogID: blogID).method,
-//            headers: NetworkRouter.getArticlesInBlog(blogID: blogID).headers
-//        )
-//        // URL에 파라미터 추가
-//        if let url = urlRequest.url {
-//            var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
-//            components?.queryItems = [
-//                URLQueryItem(name: "page", value: "\(page)")
-//            ]
-//            urlRequest.url = components?.url
-//        }
-//        
-//        let response = try await AF.request(
-//            urlRequest,
-//            interceptor: NetworkInterceptor()
-//        ).validate().serializingDecodable([Post].self).value
-//        
-//        return response
-//    }
-//}
-
-
 import Foundation
 import Alamofire
 
@@ -306,10 +168,7 @@ final class NetworkRepository {
         .serializingData()
         .value
         
-        // 응답 내용 출력
-        if let responseString = String(data: response, encoding: .utf8) {
-            print("서버 응답:", responseString)
-        }
+        logResponse(response, url: urlRequest.url?.absoluteString ?? "unknown")
     }
     
     func getArticlesInBlog(blogID: Int, page: Int) async throws -> [Post] {
@@ -329,19 +188,21 @@ final class NetworkRepository {
         
         logRequest(urlRequest)
         
-        // 먼저 응답 데이터를 확인해보기
+        // ISO8601DateFormatter로 날짜 처리
+        let decoder = JSONDecoder()
+        
+        // DateFormatter를 사용하여 ISO8601 형식을 맞추기
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        
         let response = try await AF.request(
             urlRequest,
             interceptor: NetworkInterceptor()
         ).validate()
-        .serializingData()
+        .serializingDecodable(PostListDto.self, decoder: decoder)
         .value
         
-        if let responseString = String(data: response, encoding: .utf8) {
-            print("서버 응답:", responseString)
-        }
-        
-        // 실제 응답 형식을 확인한 후 디코딩
-        return try JSONDecoder().decode(PostListDto.self, from: response).articles
+        return response.articles
     }
 }
