587950a784ca9e7a2e37a245c0a9c4ff0dfed2f4
muzigae <mujigae1020@gmail.com>
2025-01-26 03:39:11 +0900

    Feat: Create deep link handler for kakao auth

---
+++ Wastory/Wastory/DeepLinkHandler.swift
@@ -0,0 +1,26 @@
+//
+//  DeepLinkHandler.swift
+//  Wastory
+//
+//  Created by mujigae on 1/26/25.
+//
+
+import Foundation
+
+@MainActor
+struct DeepLinkHandler {
+    static let shared = DeepLinkHandler()    // 싱글톤 인스턴스
+    
+    func authHandler(url: URL) async {
+        guard url.scheme == "wastory",
+              url.host == "authSuccess",
+              let accessToken = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems?.first(where: { $0.name == "access_token" })?.value,
+              let refreshToken = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems?.first(where: { $0.name == "refresh_token" })?.value
+        else {
+            print("Error: Failed to fetch jwt token for kakao users")
+            return
+        }
+        NetworkConfiguration.accessToken = accessToken
+        NetworkConfiguration.refreshToken = refreshToken
+    }
+}
