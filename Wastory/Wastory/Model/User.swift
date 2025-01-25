75d5961f5207faa66e15f0222ed30cb1f055a757
muzigae <mujigae1020@gmail.com>
2025-01-26 03:46:28 +0900

    Feat: Add user model coding keys

--- Wastory/Wastory/Model/User.swift
+++ Wastory/Wastory/Model/User.swift
@@ -8,13 +8,20 @@
 import Foundation
 
 struct User: Codable {
-    
-    let userID: String              // 이메일 주소
-    var password: String            // 비밀번호
-    var username: String            // 유저네임
+    let userID: String               // 이메일 주소
+    var password: String             // 비밀번호
+    var username: String             // 유저네임
     
     let blogID: UUID                 // 블로그 주소
     var subscribedBlogIDs: [UUID]    // 구독중인 블로그 주소 List
     var subscriberBlogIDs: [UUID]    // 구독자 블로그 주소 List
     
+    private enum CodingKeys: String, CodingKey {
+        case userID = "email"
+        case password
+        case username
+        case blogID
+        case subscribedBlogIDs
+        case subscriberBlogIDs
+    }
 }
