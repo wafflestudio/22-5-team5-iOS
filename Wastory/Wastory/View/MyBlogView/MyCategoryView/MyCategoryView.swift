155bc2f5e6e6e309d66633f791f8bf1b81003a77
JoongWon SHIN <joongwon0204@gmail.com>
2025-01-29 11:42:01 +0900

    Feat: Prevent Category overlapping

--- Wastory/Wastory/View/MyBlogView/MyCategoryView/MyCategoryView.swift
+++ Wastory/Wastory/View/MyBlogView/MyCategoryView/MyCategoryView.swift
@@ -149,6 +149,9 @@ struct MyCategoryView: View {
             }
         } //toolbar
         .navigationBarBackButtonHidden()
+        .alert("카테고리 이름은 중복이 불가합니다", isPresented: $viewModel.isOverlapAlertPresent) {
+            Button("확인", role: .cancel) {}
+        }
     }
 }
 
